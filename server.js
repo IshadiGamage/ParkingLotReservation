87828a6d0846a98c76dd0c3e08a110b57ec37b07
Gamage <UGAMAIS@PEROOT.COM>
2017-12-18 16:37:34 +0530

    adding serach bar to the sign up page

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/',function (req,res) {
 });
 */
 
-app.use(express.static(__dirname +"/interfaces"));
+app.use(express.static(__dirname +"/public"));
 
 app.listen(3000);
 console.log('App is running on port 3000');
